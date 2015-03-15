26cb2737ca4207d0fb044deacda8be7d88a4207d
Che-Liang Chiou <clchiou@gmail.com>
2015-03-15 09:12:35 -0700

    Add WriteOnceDict

--- iga/core.py
+++ iga/core.py
@@ -5,6 +5,7 @@ __all__ = [
 ]
 
 from collections import OrderedDict
+from collections import MutableMapping
 
 
 class ImmutableOrderedSet:
@@ -20,3 +21,30 @@ class ImmutableOrderedSet:
 
     def __iter__(self):
         return iter(self._data)
+
+
+class WriteOnceDict(MutableMapping):
+    """A dict that does not allow overwriting keys."""
+
+    def __init__(self, iterable=None):
+        if iterable:
+            self._data = dict(iterable)
+        else:
+            self._data = {}
+
+    def __getitem__(self, key):
+        return self._data[key]
+
+    def __setitem__(self, key, value):
+        if key in self._data:
+            raise TypeError('cannot overwrite key %r' % key)
+        self._data[key] = value
+
+    def __delitem__(self, key):
+        del self._data[key]
+
+    def __iter__(self):
+        return iter(self._data)
+
+    def __len__(self):
+        return len(self._data)
---
+++ tests/core/test_write_once_dict.py
@@ -0,0 +1,21 @@
+import unittest
+
+from iga.core import WriteOnceDict
+
+
+class WriteOnceDictTest(unittest.TestCase):
+
+    def test_write_once(self):
+        d = WriteOnceDict([(1, 1), (2, 2)])
+        d[3] = 3
+        self.assertEqual(3, len(d))
+        self.assertEqual(1, d[1])
+        self.assertEqual(2, d[2])
+        self.assertEqual(3, d[3])
+        self.assertEqual([1, 2, 3], sorted(d))
+        self.assertRaises(TypeError, d.__setitem__, 1, 2)
+        self.assertRaises(TypeError, d.__setitem__, 2, 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
