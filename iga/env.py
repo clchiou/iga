b8bea4a4b33d3fecf0ea9723fc75602b9d2cb502
Che-Liang Chiou <clchiou@gmail.com>
2015-04-07 22:37:19 -0700

    Remove threading

--- iga/env.py
+++ iga/env.py
@@ -5,43 +5,31 @@ __all__ = [
 ]
 
 import contextlib
-import threading
 from collections import ChainMap
 
 from iga.core import WriteOnceDict
 
 
-_LOCAL = None
-_ROOT = None
-
-
-def _local():
-    global _LOCAL
-    if _LOCAL is None:
-        _LOCAL = threading.local()
-    if not hasattr(_LOCAL, 'current'):
-        _LOCAL.current = root()
-    return _LOCAL
-
-
-def root():
-    """Return the singleton root env object."""
-    global _ROOT
-    if _ROOT is None:
-        _ROOT = ChainMap(WriteOnceDict())
-    return _ROOT
+_ROOT = ChainMap(WriteOnceDict())
 
 
 def current():
     """Return the current env object."""
-    return _local().current
+    return current.current
+
+
+current.current = _ROOT
 
 
 @contextlib.contextmanager
 def enter_child_env():
     """Enter a child env context."""
-    local = _local()
-    child = local.current.new_child(WriteOnceDict())
-    local.current, parent = child, local.current
+    child = current.current.new_child(WriteOnceDict())
+    current.current, parent = child, current.current
     yield child
-    local.current = parent
+    current.current = parent
+
+
+def root():
+    """Return the singleton root env object."""
+    return _ROOT
