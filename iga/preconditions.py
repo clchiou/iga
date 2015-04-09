eae57d170953f9ac18a0e1c8747c24360b91e4df
Che-Liang Chiou <clchiou@gmail.com>
2015-04-08 22:24:37 -0700

    Refactor preconditions

--- iga/preconditions.py
+++ iga/preconditions.py
@@ -9,21 +9,20 @@ __all__ = [
 from iga.error import IgaError
 
 
-def check(condition, message):
+def check(condition, message, *args):
     if not condition:
-        raise IgaError(message)
+        raise IgaError(message % args)
 
 
 def check_type(var, *types):
     """Check if `var` is of one of the types."""
-    if not any(isinstance(var, typ) for typ in types):
-        raise IgaError('%r is not one of %r' % (var, types))
+    check(any(isinstance(var, typ) for typ in types),
+          '%r is not one of %r', var, types)
 
 
 def check_iterable(var, *element_types):
     """Check if `var` is iterable whose elements are of one of the types."""
     check_type(var, tuple, list, set, frozenset)
     for index, element in enumerate(var):
-        if not any(isinstance(element, typ) for typ in element_types):
-            raise IgaError('lst[%d] == %r is one of %r' %
-                           (index, element, element_types))
+        check(any(isinstance(element, typ) for typ in element_types),
+              'lst[%d] == %r is one of %r', index, element, element_types)
