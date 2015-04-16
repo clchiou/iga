5816db5d20308ef0396a3fca17b6ec8d91557b2c
Che-Liang Chiou <clchiou@gmail.com>
2015-04-15 20:44:10 -0700

    Remove unused Bimap

--- iga/core.py
+++ iga/core.py
@@ -1,10 +1,8 @@
 """Core data structures."""
 
 __all__ = [
-    'Bimap',
     'ImmutableOrderedSet',
     'OrderedSet',
-    'WriteOnceBimap',
     'WriteOnceDict',
     'group',
     'list_difference',
@@ -101,83 +99,3 @@ class WriteOnceDict(MutableMapping):
 
     def __str__(self):
         return _repr(self, self._data)
-
-
-class Bimap(MutableMapping):
-    """A dict that also allows look up by value."""
-
-    def __init__(self, *, _key_to_value=None, _value_to_key=None):
-        self._key_to_value = _key_to_value or {}
-        self._value_to_key = _value_to_key or {}
-
-    def inverse(self):
-        return Bimap(_key_to_value=dict(self._value_to_key),
-                     _value_to_key=dict(self._key_to_value))
-
-    def get_key(self, value, default=None):
-        return self._value_to_key.get(value, default)
-
-    def __getitem__(self, key):
-        return self._key_to_value[key]
-
-    def __setitem__(self, key, value):
-        if value in self._value_to_key:
-            raise KeyError('value %r already bound to key %r' %
-                           (value, self._value_to_key[value]))
-        if key in self._key_to_value:
-            del self._value_to_key[self._key_to_value[key]]
-        self._key_to_value[key] = value
-        self._value_to_key[value] = key
-
-    def __delitem__(self, key):
-        value = self._key_to_value[key]
-        del self._key_to_value[key]
-        del self._value_to_key[value]
-
-    def __iter__(self):
-        return iter(self._key_to_value)
-
-    def __len__(self):
-        return len(self._key_to_value)
-
-    def __repr__(self):
-        return _repr(self, self._key_to_value)
-
-    def __str__(self):
-        return _repr(self, self._key_to_value)
-
-
-class WriteOnceBimap(MutableMapping):
-
-    def __init__(self, *, _bimap=None):
-        self._bimap = _bimap or Bimap()
-
-    def inverse(self):
-        return WriteOnceBimap(_bimap=self._bimap.inverse())
-
-    def get_key(self, value, default=None):
-        return self._bimap.get_key(value, default)
-
-    def __getitem__(self, key):
-        return self._bimap[key]
-
-    def __setitem__(self, key, value):
-        if key in self:
-            raise KeyError('cannot overwrite key %r' % (key,))
-        self._bimap[key] = value
-
-    def __delitem__(self, key):
-        raise KeyError('cannot delete keys from %s (key=%r)' %
-                       (self.__class__.__name__, key))
-
-    def __iter__(self):
-        return iter(self._bimap)
-
-    def __len__(self):
-        return len(self._bimap)
-
-    def __repr__(self):
-        return _repr(self, dict(self._bimap))
-
-    def __str__(self):
-        return _repr(self, dict(self._bimap))
