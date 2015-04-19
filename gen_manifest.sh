bae7ece489ea69d4ee4f8b7b97791cfd1200538f
Che-Liang Chiou <clchiou@gmail.com>
2015-04-19 00:09:54 -0700

    Fix gen_manifest.sh not overwrite when not a git repo

--- gen_manifest.sh
+++ gen_manifest.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-git ls-tree -r --name-only HEAD \
-  | grep -v '^iga\|^\.' \
-  | awk '{print "include " $0}' \
-  > MANIFEST.in
+if [ -d .git ]; then
+  git ls-tree -r --name-only HEAD \
+    | grep -v '^iga\|^\.' \
+    | awk '{print "include " $0}' \
+    > MANIFEST.in
+fi
